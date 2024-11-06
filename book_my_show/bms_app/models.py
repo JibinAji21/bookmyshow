32da248031fa2f4537b9a195660f596d37dba9ec
JibinAji21 <jibinaji26@gmail.com>
2024-11-06 17:01:31 +0530

    Book_My_Show_Django

--- book_my_show/bms_app/models.py
+++ book_my_show/bms_app/models.py
@@ -3,18 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# class Movie(models.Model):
-#     movie_name=models.TextField()
-#     bg_img=models.FileField()
-#     fg_img=models.FileField()
-#     duration=models.TextField()
-#     category=models.TextField()
-#     date=models.DateField()
+class Movie(models.Model):
+    movie_name=models.TextField()
+    bg_img=models.FileField()
+    fg_img=models.FileField()
+    duration=models.TextField()
+    category=models.TextField()
+    date=models.DateField()
 
-# class Language(models.Model):
-#     movie_language=models.TextField()
+class Language(models.Model):
+    movie_language=models.TextField()
 
-# class Movie_lang(models.Model):
-#     movie=models.ForeignKey(Movies,on_delete=models.CASCADE)
-#     lang=models.ForeignKey(Language,on_delete=models.CASCADE)
+class Movie_lang(models.Model):
+    movie=models.ForeignKey(Movie,on_delete=models.CASCADE)
+    lang=models.ForeignKey(Language,on_delete=models.CASCADE)
+    
 
